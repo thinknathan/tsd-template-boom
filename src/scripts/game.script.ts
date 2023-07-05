1e6f7fc181a3bead4fed7c9d0857d126455d047c
Nathan Bolton <nathan@nathanbolton.ca>
2023-07-04 23:15:07 -0700

    Update for latest type definitions

--- src/scripts/game.script.ts
+++ src/scripts/game.script.ts
@@ -14,25 +14,38 @@ export function init(this: Game): void {
 
 	// Run all game logic inside `boom`.
 	boom(() => {
+		const PLAYER_START_X = 120;
+		const PLAYER_START_Y = 80;
 		const player = add([
 			sprite('sloth2', { atlas: 'mainAtlas' }),
-			pos(120, 80),
+			pos(PLAYER_START_X, PLAYER_START_Y),
 			rotate(0),
+			area({
+				shape: 'auto',
+			}),
 			anchor('center'),
 		]);
 
-		// Use `utils` to satisfy TypeScript's type checker.
-		if (utils.hasRotate(player)) {
-			on_update((_cancel) => {
-				player.rotate(player.angle - 120 * dt());
-			});
-		}
+		const ROTATION_RATE = 120;
+		on_update((_cancel) => {
+			player.rotate(player.angle - ROTATION_RATE * dt());
+		});
 
+		const LOOP_MAX = 5;
 		// Run a lua-style loop 5 times.
-		for (const i of $range(1, 5)) {
+		for (const _ of $range(1, LOOP_MAX)) {
 			const x = rand(0, width());
 			const y = rand(0, height());
 			add([sprite('snake2', { atlas: 'mainAtlas' }), pos(x, y)]);
 		}
+
+		const NEW_ROTATION = 180;
+		player.on_collide('item-damage-up', (collision) => {
+			// If you have an unknown object, you can use
+			// `utils` to satisfy TypeScript's type checker.
+			if (utils.hasRotate(collision.target)) {
+				collision.target.rotate(NEW_ROTATION);
+			}
+		});
 	});
 }
